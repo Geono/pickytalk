61e8f702599250cb71023c0f85e3d1ccf9e1b0f8
Geono <syaokun2744@gmail.com>
2018-09-10 10:30:13 +0900

    Chat ui intermediate (1)

--- App.js
+++ App.js
@@ -1,24 +1,72 @@
 import React from 'react';
-import store from './app/store';
-import Home from './app/home';
-import { Provider } from 'react-redux';
-import { StyleSheet } from 'react-native';
+import { Platform } from 'react-native';
+import PropTypes from 'prop-types';
+import { GiftedChat } from 'react-native-gifted-chat';
+import emojiUtils from 'emoji-utils';
+
+import SlackMessage from './app/SlackMessage';
 
 export default class App extends React.Component {
-    render() {
+    state = {
+        messages: [],
+    };
+
+    componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: './assets/g.jpeg'
+                    },
+                },
+            ],
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    static renderMessage(props) {
+        const { currentMessage: { text: currText } } = props;
+
+        let messageTextStyle;
+
+        // Make "pure emoji" messages much bigger than plain text.
+        if (currText && emojiUtils.isPureEmojiString(currText)) {
+            messageTextStyle = {
+                fontSize: 28,
+                // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
+                lineHeight: Platform.OS === 'android' ? 34 : 30,
+            };
+        }
+
         return (
-            <Provider store={store}>
-                <Home />
-            </Provider>
+            <SlackMessage {...props} messageTextStyle={messageTextStyle} />
         );
     }
-}
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+    render() {
+        return (
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                alwaysShowSend
+                isAnimated
+                showUserAvatar
+                user={{
+                    _id: 1,
+                }}
+                renderMessage={this.renderMessage}
+            />
+        );
+    }
+
+}
