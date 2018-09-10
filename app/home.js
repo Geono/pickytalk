38f10bfba8f47a7c3294e947ed1c3f2d06639544
SUSEOK LEE <suseok110.lee@gmail.com>
2018-09-10 14:53:35 +0900

    Firebase add

--- app/home.js
+++ app/home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+
+import readUsers from './firebase/firebase';
 
 class Home extends React.Component {
 
-    render() {
-        return <View>
-            <Text>Hello world!</Text>
-        </View>
-    }
+	render() {
+		readUsers();
+		return (
+			<View>
+				<Text>Hello world!</Text>
+				<Text>Hello world!sdfsdfsesfssfesdfasdf</Text>
+			</View>
+		);
+	}
 }
 
 
