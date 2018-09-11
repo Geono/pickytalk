e974ccf548529cb2d1382ff07326494241b16fa3
Geono <syaokun2744@gmail.com>
2018-09-11 13:22:28 +0900

    Message fetch

--- app/chat-list/index.js
+++ app/chat-list/index.js
@@ -1,34 +1,63 @@
 import * as React from 'react';
 import { View } from 'react-native';
 import PropTypes from 'prop-types';
+import { List } from 'react-native-paper';
+import { createStackNavigator } from 'react-navigation';
+import ChatScreen from '../chat-screen';
 import Header from '../header';
-import {getDb} from '../firebase/firebase';
+import { getDb } from '../firebase/firebase';
 
-export default class ChatList extends React.Component {
+class ChatList extends React.Component {
+    static navigationOptions = {
+        header: {
+            visible: false,
+        }
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: []
+        };
+    }
+
     componentDidMount() {
         const conversationsList = getDb().collection('conversations-list');
-        const userInfo = this.props.userInfo;
+        const userInfo = this.props.screenProps.userInfo;
         conversationsList.get().then(querySnapshot => {
             var results = [];
-            querySnapshot.forEach(function(doc) {
+            querySnapshot.forEach(function (doc) {
                 const data = doc.data();
-                if(data.user_id &&
+                if (data.user_id &&
                     data.user_id.includes(userInfo.id)) {
                     results.push(data);
                 }
             });
-            return results;
+            return results.sort((a, b) => b.upd_date - a.upd_date);
         }).then(results => {
-            console.log(results);
+            this.setState({ list: results });
         }).catch(function (error) {
-            console.log(error);
+            console.error(error);
         });
     }
 
     render() {
         return (
             <View>
-                <Header userInfo={this.props.userInfo} />
+                <Header userInfo={this.props.screenProps.userInfo} />
+                {this.state.list !== null && this.state.list.map(data =>
+                    (<List.Item
+                        key={data.title + data.upd_date}
+                        title={data.title}
+                        description={data.last_sentence}
+                        onPress={() => {
+                            this.props.navigation.navigate('ChatScreen', {
+                                conversationId: data.conversation_id
+                            });
+                        }}
+                        left={props => <List.Icon {...props} icon="chat" />}
+                    />)
+                )}
             </View>
         );
     }
@@ -49,3 +78,44 @@ ChatList.defaultProps = {
         avatarUrl: '',
     }
 };
+
+const ChatStackNavigator = createStackNavigator(
+    {
+        ChatList: {
+            screen: ChatList,
+            headerMode: 'none',
+            header: null,
+            navigationOptions: {
+                header: null
+            }
+        },
+        ChatScreen: {
+            screen: ChatScreen,
+            headerMode: 'none',
+            header: null,
+            navigationOptions: {
+                header: null
+            }
+        },
+    },
+    {
+        initialRouteName: 'ChatList',
+        header: null,
+        headerMode: 'none',
+        navigationOptions: {
+            header: null
+        }
+    }
+);
+
+export default class ChatStack extends React.Component {
+    render() {
+        return (
+            <ChatStackNavigator
+                screenProps={{
+                    userInfo: this.props.userInfo
+                }}
+            />
+        );
+    }
+}
---
+++ app/chat-screen/index.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import { Platform, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import emojiUtils from 'emoji-utils';
+import { Appbar } from 'react-native-paper';
+import { getAvatar } from '../helper';
+import SlackMessage from './SlackMessage';
+import { getDb } from '../firebase/firebase';
+
+export default class ChatScreen extends React.Component {
+    state = {
+        messages: [],
+    };
+
+    componentWillMount() {
+        const { navigation } = this.props;
+        const conversationId = navigation.getParam('conversationId');
+
+        const query = getDb()
+            .collection('conversations')
+            .where('conversation_id', '==', conversationId)
+            .orderBy('createdAt', 'desc')
+            .limit(100);
+
+        const chatArr = [];
+        let messages;
+
+        // 1. Get conversations
+        query.get().then(results => {
+            if (results.empty) {
+                console.log('No history');
+            } else {
+                const userIdSet = new Set();
+                // find conversations
+                results.forEach(function (doc) {
+                    const chatData = doc.data();
+                    userIdSet.add(chatData.user_id);
+                    chatArr.push(doc.data());
+                });
+
+                // 2. Get participants of the conversation
+                chatArr.sort((a, b) => a.createdAt - b.createdAt);
+                messages = chatArr.map((chatData, index) => {
+                    return {
+                        _id: index,
+                        text: chatData.text,
+                        createdAt: chatData.createdAt,
+                        user: {
+                            _id: chatData.user_id,
+                        }
+                    }
+                });
+
+                const promises = [];
+                userIdSet.forEach(userId => {
+                    promises.push(
+                        getDb()
+                            .collection('users')
+                            .where('id', '==', userId)
+                            .get());
+                });
+                return Promise.all(promises);
+            }
+        }).then(resultsList => {
+            const userInfoMap = {};
+            resultsList.map(results => {
+                results.forEach(doc => {
+                    const userInfo = doc.data();
+                    userInfoMap[ userInfo.id ] = userInfo;
+                });
+            });
+
+            // 3. Merge userinfo into messages
+            messages = messages.map(message => {
+                const userInfoOfMessage = userInfoMap[ message.user._id ];
+                return Object.assign(message, {
+                    user: {
+                        avatar: userInfoOfMessage.avatarUrl
+                    }
+                })
+            });
+
+            this.setState({ messages });
+        }).catch(error => {
+            console.log(error);
+        });
+
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    static renderMessage(props) {
+        const { currentMessage: { text: currText } } = props;
+
+        let messageTextStyle;
+
+        // Make "pure emoji" messages much bigger than plain text.
+        if (currText && emojiUtils.isPureEmojiString(currText)) {
+            messageTextStyle = {
+                fontSize: 28,
+                // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
+                lineHeight: Platform.OS === 'android' ? 34 : 30,
+            };
+        }
+
+        return (
+            <SlackMessage {...props} messageTextStyle={messageTextStyle} />
+        );
+    }
+
+    render() {
+        console.log(this.state.messages);
+        return (
+            <View>
+                <Appbar.Header>
+                    <Appbar.BackAction
+                        onPress={() => this.props.navigation.goBack()}
+                    />
+                    <Appbar.Content
+                        title='샘플이다'
+                        subtitle='this is just a sample'
+                    />
+                </Appbar.Header>
+                <GiftedChat
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    alwaysShowSend
+                    isAnimated
+                    showUserAvatar
+                    user={{
+                        _id: 1,
+                        _avatar: getAvatar('haha')
+                    }}
+                    renderMessage={this.renderMessage}
+                />
+            </View>
+        );
+    }
+}
