e1f4bf9c7c9faf3a39bc0dd6a9a77565e3ae689f
Geono <syaokun2744@gmail.com>
2018-09-11 22:41:34 +0900

    Dashboard (1/2)

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -1,27 +1,84 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
+import { Image, View } from 'react-native';
+import { List } from 'react-native-paper';
 import Header from '../header';
 
 export default class Dashboard extends React.Component {
     render() {
-        return <Header userInfo={this.props.userInfo} />
+        return (
+            <View>
+                <Header userInfo={this.props.userInfo} />
+                <List.Accordion
+                    key="1"
+                    title="이베이코리아 G마켓"
+                    description="담당자 손혜림"
+                    left={() =>
+                        (
+                            <Image
+                                style={{ marginTop: 10, width: 32, height: 32, borderRadius: 15 }}
+                                source={require('../../assets/avatar/g.jpeg')}
+                            />
+                        )
+                    }
+                >
+                    <List.Item
+                        title="신규주문"
+                        description="10건"
+                        left={() => (
+                            <List.Icon icon="touch-app" />
+                        )}
+                    />
+                    <List.Item
+                        title="클레임"
+                        description="2건"
+                        left={() => (
+                            <List.Icon icon="priority-high" />
+                        )}
+                    />
+                    <List.Item
+                        title="고객문의"
+                        description="1건"
+                        left={() => (
+                            <List.Icon icon="question-answer" />
+                        )}
+                    />
+                </List.Accordion>
+                <List.Accordion
+                    key="2"
+                    title="이베이코리아 옥션"
+                    description="담당자 손혜림"
+                    left={() =>
+                        (
+                            <Image
+                                style={{ marginTop: 10, width: 32, height: 32, borderRadius: 15 }}
+                                source={require('../../assets/avatar/a.jpeg')}
+                            />
+                        )
+                    }
+                >
+                    <List.Item
+                        title="신규주문"
+                        description="40건"
+                        left={() => (
+                            <List.Icon icon="touch-app" />
+                        )}
+                    />
+                    <List.Item
+                        title="클레임"
+                        description="1건"
+                        left={() => (
+                            <List.Icon icon="priority-high" />
+                        )}
+                    />
+                    <List.Item
+                        title="고객문의"
+                        description="9건"
+                        left={() => (
+                            <List.Icon icon="question-answer" />
+                        )}
+                    />
+                </List.Accordion>
+            </View>
+        );
     }
 }
-
-Dashboard.propTypes = {
-    userInfo: PropTypes.shape({
-        _id: PropTypes.number,
-        user_id: PropTypes.string,
-        description: PropTypes.string,
-        avatarUrl: PropTypes.string,
-    })
-};
-
-Dashboard.defaultProps = {
-    userInfo: {
-        _id: -1,
-        user_id: '',
-        description: '',
-        avatarUrl: '',
-    }
-};
